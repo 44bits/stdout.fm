a097741bdb63296a1622cac5e0a886818bdb334e
Daegwon Kim <i-am-not-a-machine@nacyot.com>
2019-08-19 10:32:39 +0900

    플레이어 위치 이동 스크립트 개선

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 $(document).ready(function() {
 });
 
-window.getPlayer = function() {
-    var hasPlayer = !!($('.mejs__player').length)
-    if(hasPlayer) {
-        return new MediaElementPlayer($('.mejs__player')[1]);
-    } else {
-        return false;
-    }
+window.jumpPlayer = function(time) {
+    var player = new MediaElementPlayer($('.mejs__player')[1])
+    player.pause()
+    player.play()
+    player.setCurrentTime(time)
+    return true
 }
